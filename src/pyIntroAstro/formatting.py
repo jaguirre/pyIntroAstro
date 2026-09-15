730683483da736fe4b4ffc403d85bc2128f60b7f
James Aguirre <jaguirre@sas.upenn.edu>
2026-09-15 13:58:49 -0400

    Update formatting.py

--- src/pyIntroAstro/formatting.py
+++ src/pyIntroAstro/formatting.py
@@ -1,20 +1,42 @@
 from astropy import units as u
 
+def is_iterable(obj):
+    try:
+        iter(obj)
+        return True
+    except TypeError:
+        return False
+
 # Some pretty ways to print astropy units
-def Qprint(quantity_in, sigfig=3, style='s'):
+def Qprint(quantities_in, sigfig=3, style='s'):
     """ wrap up the syntax for printing astropy Quantity objects in a pretty 
     way. Other options for style inclue 'latex' """
-    quantity = quantity_in.copy()
-    # If no unit supplied, explicitly make it dimensionless
-    if not hasattr(quantity, 'unit'):
-        quantity = quantity * u.dimensionless_unscaled
-    # Need to make a zero padded string with the number of significant figures
-    sf = str(sigfig).zfill(3)
-    if style == 'hw':
-        print('Not supported')
+
+    ''' Take in a scalar or iterable (list, array) of Quantity objects
+    and return a formatted string, either scalar or list ''' 
+    
+    if not is_iterable(quantities_in):
+        # Make it iterable
+        quantities = [quantities_in.copy()]
     else:
-        fmtstr = '{0.value:0.'+sf+'g} {0.unit:'+style+'}'
-    x = fmtstr.format(quantity) 
+        quantities = quantities_in.copy()
+
+    x = []
+    for quantity in quantities:
+        #quantity = quantity_in.copy()
+        # If no unit supplied, explicitly make it dimensionless
+        if not hasattr(quantity, 'unit'):
+            quantity = quantity * u.dimensionless_unscaled
+        # Need to make a zero padded string with the number of significant figures
+        sf = str(sigfig).zfill(3)
+        if style == 'hw':
+            print('Not supported')
+        else:
+            fmtstr = '{0.value:0.'+sf+'g} {0.unit:'+style+'}'
+        x.append(fmtstr.format(quantity))
+
+    # If it's a scalar, re-extract it, otherwise leave as list
+        
     return x
 
 def Uprint(quantity,style='latex'):
